3ce4288de226982de5d891e4c3331ebed750a060
yu-yake2002 <yuyake02@outlook.com>
2023-11-23 21:58:58 +0800

    Style: improve code style

--- include/wire.h
+++ include/wire.h
@@ -29,95 +29,86 @@
  *
  ***************************************************************************/
 
-
-
 #ifndef __WIRE_H__
 #define __WIRE_H__
 
-#include "basic_circuit.h"
-#include "component.h"
-#include "parameter.h"
-#include "assert.h"
-#include "cacti_interface.h"
 #include <iostream>
 #include <list>
 
-class Wire : public Component
-{
-  public:
-    Wire(enum Wire_type wire_model, double len /* in u*/,
-         int nsense = 1/* no. of sense amps connected to the low-swing wire */,
-         double width_scaling = 1,
-         double spacing_scaling = 1,
-         enum Wire_placement wire_placement = outside_mat,
-         double resistivity = CU_RESISTIVITY,
-         /*TechnologyParameter::*/DeviceType *dt = &(g_tp.peri_global));
-    ~Wire();
+#include "assert.h"
+#include "basic_circuit.h"
+#include "cacti_interface.h"
+#include "component.h"
+#include "parameter.h"
 
-    Wire( double width_scaling = 1,
-         double spacing_scaling = 1,
-         enum Wire_placement wire_placement = outside_mat,
-         double resistivity = CU_RESISTIVITY,
-         /*TechnologyParameter::*/DeviceType *dt = &(g_tp.peri_global)
-    ); // should be used only once for initializing static members
-    void init_wire();
+class Wire : public Component {
+ public:
+  Wire(enum Wire_type wire_model, double len /* in u*/,
+       int nsense = 1 /* no. of sense amps connected to the low-swing wire */,
+       double width_scaling = 1, double spacing_scaling = 1,
+       enum Wire_placement wire_placement = outside_mat,
+       double resistivity = CU_RESISTIVITY,
+       /*TechnologyParameter::*/ DeviceType *dt = &(g_tp.peri_global));
+  ~Wire();
 
-    void calculate_wire_stats();
-    void delay_optimal_wire();
-    double wire_cap(double len, bool call_from_outside=false);
-    double wire_res(double len);
-    void low_swing_model();
-    double signal_fall_time();
-    double signal_rise_time();
-    double sense_amp_input_cap();
+  Wire(double width_scaling = 1, double spacing_scaling = 1,
+       enum Wire_placement wire_placement = outside_mat,
+       double resistivity = CU_RESISTIVITY,
+       /*TechnologyParameter::*/ DeviceType *dt =
+           &(g_tp.peri_global));  // should be used only once for initializing
+                                  // static members
+  void init_wire();
 
-    enum Wire_type wt;
-    double wire_spacing;
-    double wire_width;
-    enum Wire_placement wire_placement;
-    double repeater_size;
-    double repeater_spacing;
-    double wire_length;
-    double in_rise_time, out_rise_time;
+  void calculate_wire_stats();
+  void delay_optimal_wire();
+  double wire_cap(double len, bool call_from_outside = false);
+  double wire_res(double len);
+  void low_swing_model();
+  double signal_fall_time();
+  double signal_rise_time();
+  double sense_amp_input_cap();
 
-    void set_in_rise_time(double rt)
-    {
-      in_rise_time = rt;
-    }
-    static Component global;
-    static Component global_5;
-    static Component global_10;
-    static Component global_20;
-    static Component global_30;
-    static Component low_swing;
-    static double wire_width_init;
-    static double wire_spacing_init;
-    void print_wire();
+  enum Wire_type wt;
+  double wire_spacing;
+  double wire_width;
+  enum Wire_placement wire_placement;
+  double repeater_size;
+  double repeater_spacing;
+  double wire_length;
+  double in_rise_time, out_rise_time;
 
-  private:
+  void set_in_rise_time(double rt) { in_rise_time = rt; }
+  static Component global;
+  static Component global_5;
+  static Component global_10;
+  static Component global_20;
+  static Component global_30;
+  static Component low_swing;
+  static double wire_width_init;
+  static double wire_spacing_init;
+  void print_wire();
 
-    int nsense; // no. of sense amps connected to a low-swing wire if it
-                // is broadcasting data to multiple destinations
-    // width and spacing scaling factor can be used
-    // to model low level wires or special
-    // fat wires
-    double w_scale, s_scale;
-    double resistivity;
-    powerDef wire_model (double space, double size, double *delay);
-    list <Component> repeated_wire;
-    void update_fullswing();
-    static int initialized;
+ private:
+  int nsense;  // no. of sense amps connected to a low-swing wire if it
+               // is broadcasting data to multiple destinations
+  // width and spacing scaling factor can be used
+  // to model low level wires or special
+  // fat wires
+  double w_scale, s_scale;
+  double resistivity;
+  powerDef wire_model(double space, double size, double *delay);
+  list<Component> repeated_wire;
+  void update_fullswing();
+  static int initialized;
 
+  // low-swing
+  Component transmitter;
+  Component l_wire;
+  Component sense_amp;
 
-    //low-swing
-    Component transmitter;
-    Component l_wire;
-    Component sense_amp;
+  double min_w_pmos;
 
-    double min_w_pmos;
-
-    /*TechnologyParameter::*/DeviceType *deviceType;
-
+  /*TechnologyParameter::*/ DeviceType *deviceType;
 };
 
 #endif
